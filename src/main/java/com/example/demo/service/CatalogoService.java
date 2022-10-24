3844ebd9b824bc432d023ee727345cd0a56d7d5f
Alexander <72616977ertus.edu.pe>
2022-10-23 21:43:46 -0500

    avance catalogo

---
+++ src/main/java/com/example/demo/service/CatalogoService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Catalogo;
+
+	
+	@Service
+	public class CatalogoService implements ICatalogoService {
+		
+		@Override
+		public void create() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void update() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Catalogo getId(int id) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ArrayList<Catalogo> get() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
