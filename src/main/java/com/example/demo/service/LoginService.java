543f0e772d8b9b3766725b7ea3dfb7729d7da6ab
Andre-CruzGonzales <81603849+Andre-CruzGonzales@users.noreply.github.com>
2022-10-24 07:23:45 -0500

    Creacion de servicio

---
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+public class LoginService implements ILoginService{
+
+	@Override
+	public void login() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
