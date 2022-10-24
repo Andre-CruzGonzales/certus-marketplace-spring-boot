3df5bde8275df713e0409677c2bb2dfd2b993d72
Alexander <72616977ertus.edu.pe>
2022-10-23 21:46:33 -0500

    avance catalogo2

--- src/main/java/com/example/demo/controller/CatalogoController.java
+++ src/main/java/com/example/demo/controller/CatalogoController.java
@@ -1,5 +1,22 @@
 package com.example.demo.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.ICatalogoService;
+
+
+@Controller
+@RequestMapping("/catalogo")
 public class CatalogoController {
+	@Autowired
+	private ICatalogoService catalogoService;
+	@GetMapping("/create")
+	public void create() {
+		
+	}
 
 }
