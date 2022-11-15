c39df9a9e7ccc784f6623000a4d6b05294a63ea3
Alexander <72616977@certus.edu.pe>
2022-11-14 22:00:02 -0600

    avance2

---
+++ src/main/java/com/example/demo/service/ProductoServicio.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Producto;
+import com.example.demo.repository.ProductoRepositorio;
+
+@Service
+public class ProductoServicio {
+	
+	@Autowired
+	private ProductoRepositorio productoRepositorio;
+	
+	public List<Producto> listAll(){
+		return productoRepositorio.findAll();
+		
+	}
+	
+	public void save(Producto producto) {
+		productoRepositorio.save(producto);
+	
+	}
+	
+	public Producto get(Long id) {
+		return productoRepositorio.findById(id).get();
+	}
+	
+	public void delete (Long id) {
+		productoRepositorio.deleteById(id);
+	}
+
+
+}
